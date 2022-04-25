a4388d33730b3971a39e1f8d3ecf8559ad481e39
Eric <ngotuan911@gmail.com>
2022-04-24 22:40:42 -0700

    Added for custom domain

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from "styled-components";
 import {  LightTheme } from "./components/Themes";
 import GlobalStyle from "./GlobalStyles";
 import Main from './components/Main'
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import SkillsPage from "./components/SkillsPage";
 import WorkPage from "./components/WorkPage";
@@ -16,7 +16,8 @@ function App() {
       <ThemeProvider theme={LightTheme}>
 
 
-            <SlideRoutes><Route  path="/" element={<Main/>} />
+          <SlideRoutes >
+              <Route path="/" element={<Main />} />
               <Route  path="/about" element={<About/>} />
               <Route  path="/Skills" element={<SkillsPage/>} />
               <Route path="/Work" element={<WorkPage />} />
